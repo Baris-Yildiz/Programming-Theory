ffd3c3012e23266d772810dd96d389dccb652bac
Barış <barislegomaster@gmail.com>
2023-08-07 13:48:49 +0300

    UI added
    text and buttons added

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuUIHandler : MonoBehaviour
+{
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#endif
+        Application.Quit();
+
+    }
+}
